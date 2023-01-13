7b47366c5edb1918f4a038b89e70ab1be5e5ccf9
ziom <lagiewnik@gmail.com>
2023-01-13 23:28:21 +0100

    1 test changes

--- src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -3,7 +3,7 @@ package io.rockmoongames.bowlingstar;
 import org.junit.jupiter.api.Test;
 // test
 public class BowlingGameTest {
-
+//changes 123 123 123
     @Test
     public void jessicaTestOne() {
 
