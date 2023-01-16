1bfa291f4d756db7a6ddafbe3e7a543b1a98f797
Lisa <lagiewnik@gmail.com>
2023-01-16 23:14:06 +0100

    Add new class

---
+++ src/test/java/io/rockmoongames/BowlingGame2Test.java
@@ -0,0 +1,21 @@
+package io.rockmoongames.bowlingstar;
+
+import org.junit.jupiter.api.Test;
+// test
+public class BowlingGameTest {
+//changes 123 123 123
+    @Test
+    public void jessicaTestOne() {
+
+    }
+
+    @Test
+    public void jessicaTest2nd() {
+            //2nd test
+    }
+
+    @Test
+    public void lisaTest3rd() {
+            //3nd test
+    }
+}
